1d0407dc07c1a3c9fea01e0c11cd49597f631e5f
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-04-05 22:03:28 +0900

    while , Do while문법 공부

---
+++ study_210405_2/study_210405/Source.cpp
@@ -0,0 +1,79 @@
+/*
+for (int i = 0; i < 10; i++) {
+
+	}
+
+	int a = 0;
+
+	while (a<10) {
+
+
+	}
+
+	printf("hello\n");
+	a++;*/
+
+
+#include<iostream>
+/*
+while로 별구하기
+
+
+*
+**
+***
+****
+*****
+
+*/
+
+
+using namespace std;
+
+void main() {
+	/*
+	
+	while 문법
+
+	int a= 0;
+	while (a < 5) {
+		int b = 0;
+		while (b<= a) {
+			printf("*");
+			b++;
+		}
+		a++;
+		printf("\n");
+	}
+	*/
+
+
+	/*
+	do while 문법
+	
+	int a = 0;
+
+	do {
+		printf("hello world\n");
+
+	} while (a == 1) {
+		printf("hello world");
+	}
+
+
+	*/
+
+
+
+
+
+
+
+
+
+	fgetc(stdin);
+}
+
+
+
+
